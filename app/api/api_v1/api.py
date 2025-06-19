35adee702fd10fb6f94ba52e3a7c4d560d764997
“onerkerim” <onerkerim@hotmail.com>
2025-06-19 23:18:21 +0300

    Yeni rol yönetimi API'si eklendi

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import auth
+from app.api.api_v1.endpoints import auth, roles
 
 api_router = APIRouter()
 
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
+api_router.include_router(roles.router, prefix="/roles", tags=["roles"])
